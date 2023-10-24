663320d8b2c2f20ed7a8fbd545ad554609867ecf
tmq077 <142147568+tmq077@users.noreply.github.com>
2023-10-24 16:56:58 +0800

    Update foodbot.py

--- streamlit/foodbot.py
+++ streamlit/foodbot.py
@@ -8,10 +8,9 @@ import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+import json
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex
 from llama_index import download_loader
-from PIL import Image
-
 
 # In[19]:
 
