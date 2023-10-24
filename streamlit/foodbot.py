de33b717fb8681d745e08f1b99dc3642e3683f78
tmq077 <142147568+tmq077@users.noreply.github.com>
2023-10-24 16:52:36 +0800

    Update foodbot.py

--- streamlit/foodbot.py
+++ streamlit/foodbot.py
@@ -19,77 +19,50 @@ from PIL import Image
 # Set the GPT-3 api key
 openai.api_key = st.secrets.openai_key
 
-
-# In[16]:
-
-
 SimpleWebPageReader = download_loader("SimpleWebPageReader")
+loader = SimpleWebPageReader()
+documents = loader.load_data(urls=['https://www.metta.org.sg/donation-in-kind/'])
+index = GPTVectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
 
+class Chatbot:
+    def __init__(self, api_key, index):
+        self.index = index
+        openai.api_key = api_key
+        self.chat_history = []
 
-# In[6]:
+    def generate_response(self, user_input):
+        query_engine = index.as_query_engine()
+        response = query_engine.query(user_input)
 
-st.header("Chat with the Streamlit docs 💬 📚")
+        message = {"role": "assistant", "content": response.response}
+        self.chat_history.append({"role": "user", "content": user_input})
+        self.chat_history.append(message)
+        return message
 
-if "messages" not in st.session_state.keys(): # Initialize the chat message history
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Food Donation"}
-    ]
+    def load_chat_history(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                self.chat_history = json.load(f)
+        except FileNotFoundError:
+            pass
 
+    def save_chat_history(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.chat_history, f)
 
-# In[20]:
+# Create a Streamlit app
+st.title("Chatbot Application")
 
+# Initialize the Chatbot
+bot = Chatbot(api_key, index=index)
+bot.load_chat_history("chat_history.json")
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        loader = SimpleWebPageReader()
-        urls = ['https://www.foodfromtheheart.sg/in-kind-donations','https://www.metta.org.sg/donation-in-kind/']        
-        docs = loader.load_data(urls=urls)
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on Food Donations and your job is to answer questions. Assume that all questions are related to food donations."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        return index
-
-index = load_data()
-
-
-# In[21]:
-
-
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-
-# In[31]:
-
-
-# Initialize session_state if not already defined
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
-
-# Prompt for user input and save to chat history
-if prompt := st.text_input("Your question"):  
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-# Display the prior chat messages
-for message in st.session_state.messages:  
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-
-# In[26]:
-
-
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
-
-
-# In[ ]:
-
-
-
-
+user_input = st.text_input("You:", "")
+if user_input.lower() in ["bye", "goodbye"]:
+    st.write("Bot: Goodbye!")
+    bot.save_chat_history("chat_history.json")
+else:
+    if st.button("Submit"):
+        response = bot.generate_response(user_input)
+        st.write(f"Bot: {response['content']}")
