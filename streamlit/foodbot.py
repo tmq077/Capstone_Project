be5b162e0724c788c56fafc937860ef63467a06f
tmq077 <142147568+tmq077@users.noreply.github.com>
2023-11-01 18:47:44 +0800

    Update foodbot.py

--- streamlit/foodbot.py
+++ streamlit/foodbot.py
@@ -1,6 +1,7 @@
 # Import the required libraries
 import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.chat_engine import CondenseQuestionChatEngine
 from llama_index.llms import OpenAI
 import openai
 from llama_index import StorageContext, load_index_from_storage
