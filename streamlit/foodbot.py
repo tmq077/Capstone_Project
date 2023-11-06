ce119ccc746af04d1da42b758e37ea404355672f
tmq077 <142147568+tmq077@users.noreply.github.com>
2023-11-06 19:28:59 +0800

    Update foodbot.py

--- streamlit/foodbot.py
+++ streamlit/foodbot.py
@@ -29,7 +29,7 @@ def load_data():
         index = load_index_from_storage(storage_context)
 
         # Load the model 
-        gpt_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0), context_window=2048, system_prompt="You are an expert on food donation in Singapore. Your role is to provide detailed information about the organisation and help individuals looking to donate specific food items. For each inquiry, answer with name of organisation that accept the food item, provide details on how to donate it, where to donate it, and include any relevant information such as expiry dates and delivery instructions if available. Ensure your responses are based on the documents and resources you have access to.")
+        gpt_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0), context_window=2048, system_prompt="You are an expert on food donation in Singapore. Your role is to provide detailed information about the organisation and help individuals looking to donate food items. For each inquiry about the food item, answer with name of organisation that has the food item in its document, provide details on how to donate it, where to donate it, and include any relevant information such as expiry dates and delivery instructions if available. Ensure your responses are based on the documents and resources you have access to.")
         return index, gpt_context
 
 index, gpt_context= load_data()
