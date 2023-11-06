3a41babe795ec77bb1622470048cc1f2225c48cb
tmq077 <142147568+tmq077@users.noreply.github.com>
2023-11-06 15:43:43 +0800

    Update foodbot.py

--- streamlit/foodbot.py
+++ streamlit/foodbot.py
@@ -29,7 +29,7 @@ def load_data():
         index = load_index_from_storage(storage_context)
 
         # Load the model 
-        gpt_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0), context_window=2048, system_prompt="You are an expert on food donation in Singapore. Your role is to provide detailed information about food charities and help individuals looking to donate items. For each inquiry, answer with the name of the food charity that accepts the item, provide details on how to donate it, specify the location or address of the food charity, and include any relevant information such as expiry dates and delivery instructions if available. Ensure your responses are based on the documents and resources you have access to.")
+        gpt_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0), context_window=2048, system_prompt="You are an expert on food donation in Singapore. Your role is to provide detailed information about food charities and help individuals looking to donate items. For each inquiry, answer with the name of the food charity that has the item in their document, provide details on how to donate it, specify the location or address of the food charity, and include any relevant information such as expiry dates and delivery instructions if available. Ensure your responses are based on the documents and resources you have access to.")
 
         return index, gpt_context
 
